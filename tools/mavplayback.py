12c94509ca817b4a4585460f91557bcb39eb23c7
Andrew Tridgell <tridge@samba.org>
2011-12-19 14:43:48 +1100

    improved playback GUI

--- tools/mavplayback.py
+++ tools/mavplayback.py
@@ -8,7 +8,7 @@ Useful for visualising flights
 '''
 
 import sys, time, os, struct
-import Tkinter
+import Tkinter, PIL
 
 # allow import from the parent directory, where mavlink.py is
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.realpath(__file__)), '..'))
@@ -19,7 +19,6 @@ from optparse import OptionParser
 parser = OptionParser("mavplayback.py [options]")
 
 parser.add_option("--planner",dest="planner", action='store_true', help="use planner file format")
-parser.add_option("--robust",dest="robust", action='store_true', help="Enable robust parsing (skip over bad data)")
 parser.add_option("--condition",dest="condition", default=None, help="select packets by condition")
 parser.add_option("--gpsalt", action='store_true', default=False, help="Use GPS altitude")
 parser.add_option("--mav10", action='store_true', default=False, help="Use MAVLink protocol 1.0")
@@ -39,12 +38,24 @@ if len(args) < 1:
 
 filename = args[0]
 
+
+def LoadImage(filename):
+    '''return an image from the images/ directory'''
+    from PIL import ImageTk, Image
+    app_dir = os.path.dirname(os.path.realpath(__file__))
+    path = os.path.join(app_dir, 'images', filename)
+    return ImageTk.PhotoImage(Image.open(path))
+
+
 class App():
     def __init__(self, filename):
         self.root = Tkinter.Tk()
 
+        self.filesize = os.path.getsize(filename)
+        self.filepos = 0.0
+
         self.mlog = mavutil.mavlink_connection(filename, planner_format=opts.planner,
-                                               robust_parsing=opts.robust)
+                                               robust_parsing=True)
         self.mout = []
         for m in opts.out:
             self.mout.append(mavutil.mavudp(m, input=False))
@@ -60,47 +71,123 @@ class App():
             sys.exit(1)
         self.last_timestamp = getattr(self.msg, '_timestamp')
 
+        self.paused = False
+
+        self.topframe = Tkinter.Frame(self.root)
+        self.topframe.pack(side=Tkinter.TOP)
+
         self.frame = Tkinter.Frame(self.root)
-        self.frame.pack()
+        self.frame.pack(side=Tkinter.LEFT)
 
-        self.quit = Tkinter.Button(self.frame, text="QUIT", command=self.frame.quit)
-        self.quit.pack(side=Tkinter.LEFT)
+        self.slider = Tkinter.Scale(self.topframe, from_=0, to=1.0, resolution=0.01,
+                                    orient=Tkinter.HORIZONTAL, command=self.slew)
+        self.slider.pack(side=Tkinter.LEFT)
 
-        self.clock = Tkinter.Label(self.frame,text="")
-        self.clock.pack()
+        self.clock = Tkinter.Label(self.topframe,text="")
+        self.clock.pack(side=Tkinter.RIGHT)
 
-        self.playback = Tkinter.Spinbox(self.frame, from_=0, to=20, increment=0.5)
-        self.playback.pack()
+        self.playback = Tkinter.Spinbox(self.topframe, from_=0, to=20, increment=0.5, width=3)
+        self.playback.pack(side=Tkinter.BOTTOM)
         self.playback.delete(0, "end")
         self.playback.insert(0, 1)
 
+        self.buttons = {}
+        self.button('quit', 'gtk-quit.png', self.frame.quit)
+        self.button('pause', 'media-playback-pause.png', self.pause)
+        self.button('rewind', 'media-seek-backward.png', self.rewind)
+        self.button('forward', 'media-seek-forward.png', self.forward)
+
         self.next_message()
         self.root.mainloop()
+
+    def button(self, name, filename, command):
+        '''add a button'''
+        img = LoadImage(filename)
+        b = Tkinter.Button(self.frame, image=img, command=command)
+        b.image = img
+        b.pack(side=Tkinter.LEFT)
+        self.buttons[name] = b
+        
+
+    def pause(self):
+        '''pause playback'''
+        self.paused = not self.paused
+
+    def rewind(self):
+        '''rewind 10%'''
+        pos = int(self.mlog.f.tell() - 0.1*self.filesize)
+        if pos < 0:
+            pos = 0
+        self.mlog.f.seek(pos)
+        self.find_message()
+
+    def forward(self):
+        '''forward 10%'''
+        pos = int(self.mlog.f.tell() + 0.1*self.filesize)
+        if pos > self.filesize:
+            pos = self.filesize - 2048
+        self.mlog.f.seek(pos)
+        self.find_message()
+
+
+    def find_message(self):
+        '''find the next valid message'''
+        while True:
+            self.msg = self.mlog.recv_match(condition=opts.condition)
+            if self.msg is not None and self.msg.get_type() != 'BAD_DATA':
+                break
+            if self.mlog.f.tell() > self.filesize - 10:
+                self.paused = True
+                break
+        self.last_timestamp = getattr(self.msg, '_timestamp')
+
+    def slew(self, value):
+        '''move to a given position in the file'''
+        if float(value) != self.filepos:
+            pos = float(value) * self.filesize
+            self.mlog.f.seek(int(pos))
+            self.find_message()
+
 
     def next_message(self):
         '''called as each msg is ready'''
         
         msg = self.msg
         if msg is None:
-            return
+            self.paused = True
 
-        speed = float(self.playback.get())
-        if speed == 0.0:
+        if self.paused:
             self.root.after(100, self.next_message)
             return
 
+        speed = float(self.playback.get())
         timestamp = getattr(msg, '_timestamp')
 
         now = time.strftime("%H:%M:%S", time.localtime(timestamp))
         self.clock.configure(text=now)
 
-        self.root.after(int(1000*(timestamp - self.last_timestamp) / speed), self.next_message)
+        if speed == 0.0:
+            self.root.after(200, self.next_message)
+        else:
+            self.root.after(int(1000*(timestamp - self.last_timestamp) / speed), self.next_message)
         self.last_timestamp = timestamp
-        self.msg = self.mlog.recv_match(condition=opts.condition)
 
-        for m in self.mout:
-            m.write(struct.pack('>Q', timestamp*1.0e6))
-            m.write(msg.get_msgbuf().tostring())
+        while True:
+            self.msg = self.mlog.recv_match(condition=opts.condition)
+            if self.msg is None and self.mlog.f.tell() > self.filesize - 10:
+                self.paused = True
+                return
+            if self.msg is not None and self.msg.get_type() != "BAD_DATA":
+                break
+        
+        pos = float(self.mlog.f.tell()) / self.filesize
+        self.slider.set(pos)
+        self.filepos = self.slider.get()
+
+        if msg.get_type() != "BAD_DATA":
+            for m in self.mout:
+                m.write(struct.pack('>Q', timestamp*1.0e6))
+                m.write(msg.get_msgbuf().tostring())
 
         if msg.get_type() == "GPS_RAW":
             self.fdm.set('latitude', msg.lat, units='degrees')
@@ -129,13 +216,13 @@ class App():
             self.fdm.set("elevator",      msg.chan2_scaled*0.0001)
             self.fdm.set('rpm',           msg.chan3_scaled*0.01)
 
+        if msg.get_type() == 'STATUSTEXT':
+            print("APM: %s" % msg.text)
+
         if msg.get_type() == "BAD_DATA":
             if mavutil.all_printable(msg.data):
                 sys.stdout.write(msg.data)
                 sys.stdout.flush()
-
-        if msg.get_type() == 'STATUSTEXT':
-            print("APM: %s" % msg.text)
 
         if self.fdm.get('latitude') != 0:
             for f in self.fgout:
